cd525312bbe958c878e5f3eabe1f1526d597d314
Yura Antoshchenko <antoshchenko1@gmail.com>
2020-10-29 17:40:10 +0300

    try

--- app/src/androidTest/java/com/noxx/testapplication/kaspresso/testcase/auth/LoginTests.kt
+++ app/src/androidTest/java/com/noxx/testapplication/kaspresso/testcase/auth/LoginTests.kt
@@ -1,26 +1,36 @@
 package com.noxx.testapplication.kaspresso.testcase.auth
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.rule.ActivityTestRule
 import com.noxx.testapplication.kaspresso.scenario.OpenAuthScreen
 import com.noxx.testapplication.kaspresso.screen.main.AuthScreen
 import com.noxx.testapplication.kaspresso.testcase.BaseTest
 import com.noxx.testapplication.ui.MainActivity
+import io.qameta.allure.android.runners.AllureAndroidJUnit4
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AllureAndroidJUnit4::class)
 internal class LoginTests : BaseTest() {
 
     private val login = "autotest"
     private val password = "autotest"
     private val wrongPassword = "1"
 
-    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+//    @get:Rule
+//    val activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    @Before
+    fun setup() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
 
     @Test
     fun loginWithEmptyLoginPassword() {
         before("Авторизация без логина и пароля") {
-            activityRule.launchActivity(null)
+            //activityRule.launchActivity(null)
         }.after {
             /* передать функцию очиски кеша */
         }.run {
@@ -37,7 +47,7 @@ internal class LoginTests : BaseTest() {
     @Test
     fun loginWithEmptyPassword() {
         before("Авторизация без пароля") {
-            activityRule.launchActivity(null)
+            //activityRule.launchActivity(null)
         }.after {
             /* передать функцию очиски кеша */
         }.run {
@@ -54,7 +64,7 @@ internal class LoginTests : BaseTest() {
     @Test
     fun loginWithWrongPassword() {
         before("Авторизация без логина и пароля") {
-            activityRule.launchActivity(null)
+            //activityRule.launchActivity(null)
         }.after {
             /* передать функцию очиски кеша */
         }.run {
@@ -71,7 +81,7 @@ internal class LoginTests : BaseTest() {
     @Test
     fun loginWithSuccessLoginPassword() {
         before("Авторизация без логина и пароля") {
-            activityRule.launchActivity(null)
+            //activityRule.launchActivity(null)
         }.after {
             /* передать функцию очиски кеша */
         }.run {
